0ce1c258647fbafa66badd414e941fd6d8c35937
eyob <eyobhabtie@gmail.com>
2026-02-26 15:34:26 +0300

    Account_Service Frist Version

---
+++ Backend/account-service/src/main/java/com/account_service/request/CreateAccountRequest.java
@@ -0,0 +1,22 @@
+package com.account_service.request;
+
+import com.account_service.entity.AccountStatus;
+import com.account_service.entity.AccountType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateAccountRequest {
+    private String accountNumber;
+    private AccountType accountType;
+    private BigDecimal balance;
+    //private Double interestRate;
+    private AccountStatus status;
+    private LocalDateTime opendAt;
+}
